6020205026b2be62a102ab33e8082eb088ec9c1c
ayushgitt <ayush795592@gmail.com>
2024-11-28 23:50:11 +0530

    modified:   client/package-lock.json modified:   client/package.json modified:   client/src/App.jsx new file:   client/src/components/LoginForm.jsx new file:   client/src/components/RegisterForm.jsx modified:   client/src/pages/Authentication.page.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,47 @@
+import React, { useState } from 'react';
 import {
-    BrowserRouter as Router,
-    Routes, Route
+  BrowserRouter as Router,
+  Routes, Route, Navigate
 } from 'react-router-dom';
 import Home from './pages/Home.page';
 import Authentication from './pages/Authentication.page';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 const App = () => {
-    return (
-        <Router>
-            <Header />
-            <Routes>
-                <Route exact path='/' element={<Home />} />
-                <Route path='/auth' element={<Authentication />} />
-            </Routes>
-            <Footer />
-        </Router>
-    );
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleSwitchToRegister = () => {
+    setIsLogin(false);
+  };
+
+  const handleSwitchToLogin = () => {
+    setIsLogin(true);
+  };
+
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/auth' element={
+          <Authentication>
+            {isLogin ? (
+              <LoginForm onSwitchToRegister={handleSwitchToRegister} />
+            ) : (
+              <RegisterForm onSwitchToLogin={handleSwitchToLogin} />
+            )}
+          </Authentication>
+        } />
+        <Route path='/login' element={<Navigate to="/auth" />} />
+        <Route path='/register' element={<Navigate to="/auth" replace state={{ isLogin: false }} />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
+
--- client/src/pages/Authentication.page.jsx
+++ client/src/pages/Authentication.page.jsx
@@ -1,9 +1,18 @@
-const Authentication = () => {
-    return (
-        <div className="">
-            Auth Page
-        </div>
-    );
-}
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Authentication = ({ children }) => {
+  const location = useLocation();
+  const isLogin = location.state?.isLogin ?? true;
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      {React.Children.map(children, child =>
+        React.cloneElement(child, { isLogin })
+      )}
+    </div>
+  );
+};
 
 export default Authentication;
+
